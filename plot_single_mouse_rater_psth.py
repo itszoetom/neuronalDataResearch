16f922fe6c3500716a7ad1ca82e4ac3618eb9c5c
itszoetom <140276679+itszoetom@users.noreply.github.com>
2024-10-19 16:09:14 -0700

    Update plot_single_mouse_rater_psth.py

--- plot_single_mouse_rater_psth.py
+++ plot_single_mouse_rater_psth.py
@@ -1,10 +1,10 @@
+# Calculating firing rate and plotting Raster and PSTH Graphs. Individual neurons in a mouse on a certain day.
+
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import jaratoolbox
 from jaratoolbox import celldatabase, ephyscore, behavioranalysis, extraplots
-
-# Calculating firing rate and plotting Raster and PSTH Graphs. Individual neurons in a mouse on a certain day.
 
 # Loads in dataframe for one mouse
 oneMouseDf = jaratoolbox.celldatabase.generate_cell_database_from_subjects(["feat004"])
